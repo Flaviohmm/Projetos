805e8e9e9ece9ff2604fe78a93f8bb35ba7cc3a9
Flaviohmm <fhenrique609@gmail.com>
2014-08-27 23:08:51 -0300

    Do while.cpp

---
+++ Do while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main ()
+{
+	string str;
+	do
+    {
+		cout <<"Digite o texto: ";
+		getline (cin, str);
+		cout << "Digitado: " << str << '\n';
+    }
+    while (str != "Adeus");
+
+	return 0;
+}
