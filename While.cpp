7c59d824da642b6e0337f9688a7510ffe33f04f5
Flaviohmm <fhenrique609@gmail.com>
2014-08-27 23:12:07 -0300

    While.cpp

---
+++ While.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+
+	int n = 10;
+	
+	while (n>0) 
+	{
+		cout << n << ", ";
+		--n;
+	}
+	
+	cout << "liftoof!\n";
+	
+	return 0;	
+		
+}
